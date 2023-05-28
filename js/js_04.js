cc83d69333d02926d7561989eb9bb24b5305fc07
soma0078 <104878713+soma0078@users.noreply.github.com>
2023-05-28 23:28:05 +0900

    5일차

---
+++ js/js_04.js
@@ -0,0 +1,44 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.last_result');
+const lowOrHi = document.querySelector('.low_or_hi');
+
+const guessSubmit = document.querySelector('.guess_submit');
+const guessField = document.querySelector('.guess_field');
+
+let guessCount = 1; 
+let resetbutton;
+
+
+function checkGuess() {
+    const userGuess = Number(guessField.value);
+    if (guessCount === 1) {
+      guesses.textContent = 'Previous guesses: ';
+    }
+    guesses.textContent += userGuess + ' ';
+  
+    if (userGuess === randomNumber) {
+      lastResult.textContent = 'Congratulations! You got it right!';
+      lastResult.style.backgroundColor = 'green';
+      lowOrHi.textContent = '';
+      setGameOver();
+    } else if (guessCount === 10) {
+      last_result.textContent = '!!!GAME OVER!!!';
+      lowOrHi.textContent = '';
+      setGameOver();
+    } else {
+      lastResult.textContent = 'Wrong!';
+      lastResult.style.backgroundColor = 'red';
+      if(userGuess < randomNumber) {
+        lowOrHi.textContent = 'Last guess was too low!';
+      } else if(userGuess > randomNumber) {
+        lowOrHi.textContent = 'Last guess was too high!';
+      }
+    }
+  
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
+  }
+checkGuess();
